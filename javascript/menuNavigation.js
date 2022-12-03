8c2a3c798df891751a483b0a661efd3c9f9f2d8a
Alerrom <alex2002andr@mail.ru>
2022-12-03 20:51:55 +0300

    feat: add highlighting menu element via js

--- javascript/menuNavigation.js
+++ javascript/menuNavigation.js
@@ -1,9 +1,33 @@
 function scaleMenu() {
     var header = document.getElementById("menu_list_id");
     if (header.className === "menu_list") {
-        header.className += " responsive";
+        header.classList.add("responsive");
     }
     else {
         header.className = "menu_list";
+    }
+}
+
+function highlightMenuElemEdit() {
+    var header = document.getElementById("menu_list_id").getElementsByTagName("a");
+    var url = document.location.href;
+    for (var i = 0; i < header.length; i++) {
+        if (url == header[i].href) {
+            header[i].classList.add("active");
+        }
+        else {
+            header[i].classList.remove("active")
+        }
+    }
+}
+
+function highlightMenuElem() {
+    var header = document.getElementById("menu_list_id").getElementsByTagName("a");
+    var currUrl = document.location.href;
+
+    for (var i = 0; i < header.length; i++) {
+        if (header[i].href == currUrl || header[i].href.slice(-5) == "#home") {
+            header[i].classList.add("active");
+        }
     }
 }

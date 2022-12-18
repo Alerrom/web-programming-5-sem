604304db65bf65ed0e09af3a505f25971af2acdb
Alerrom <alex2002andr@mail.ru>
2022-12-19 00:42:59 +0300

    feat: add handlers and functions to build table

--- javascript/config.js
+++ javascript/config.js
@@ -1,1 +1,10 @@
+document.addEventListener('DOMContentLoaded', event => loadTable(
+    document.getElementById('defaultTable'),
+    Number(localStorage.getItem('columsNum')),
+    Number(localStorage.getItem('rowsNum'))
+    ));
+
+document.addEventListener('DOMContentLoaded', event => setOnClickHandlers());
+document.addEventListener('DOMContentLoaded', event => setOnSubmitHandlers());
+
 window.addEventListener('load', event => showLoadingTime());
